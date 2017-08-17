0a1874be6feb2ca68b9d3adb0fbbe3663011de80
fanjingsong <1772995079@qq.com>
2017-08-17 10:52:05 +0800

    测试token

---
+++ application/api/validate/setBlackValidate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/17
+ * Time: 9:30
+ */
+
+namespace app\api\validate;
+
+
+class setBlackValidate extends BaseValidate
+{
+    protected $rule = [
+        'username' =>'require',
+        'username' =>'require|token',
+        'black'    =>'require',
+    ];
+
+}
